ca5577419e15c08e496f0d96f00a7a354059b55d
CodiElizabeth10 <98009702+CodiElizabeth10@users.noreply.github.com>
2022-02-12 23:22:02 -0600

    restoring and accidentally over-rid file

---
+++ Basic JaveScript Projects/Project3_math_operations/main.js
@@ -0,0 +1,30 @@
+function_addition_function() {
+    var addition=7 + 7;
+    document.getElementById("demo").innerHTML="7+7= " + addition;
+}
+
+function_subtraction() {
+    var additionsubtraction=7 - 7;
+    document.getElementById("sub").innerHTML="7-7= " - subtraction;
+}
+
+function_multiplication() {
+    var additionsubtraction=7 * 7;
+    document.getElementById("mult").innerHTML="7*7= " * multiplication;
+}
+
+function_division() {
+    var additionsubtraction=21 / 7;
+    document.getElementById("divi").innerHTML="21/7= " % division;
+}
+
+var c= 6;
+c++;
+document.write(d);
+
+var e= 5;
+e--;
+document.write(f);
+
+
+window.alert(Math.Random());
