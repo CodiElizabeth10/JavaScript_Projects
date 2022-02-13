2df90df01b6aec372dad1ba3488c92f2ce91de31
CodiElizabeth10 <98009702+CodiElizabeth10@users.noreply.github.com>
2022-02-12 21:02:47 -0600

    elements

---
+++ Basic JaveScript Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,23 @@
+function my_First_Function() {
+      var str = "I am making the button text!";
+document.getElementById("Button_Text").innerHTML=str;
+}
+
+function changeColor(newColor) {
+    var elem = document.getElementById('para', 'Button_Text');
+    elem.style.color = newColor;
+  }
+
+
+    
+  function myFunction() {
+    var sentence="I am learning";
+    sentence += "a lot from this program";
+    document.getElementById("concatenate").innerHTML=sentence;
+}
+
+
+function changeStyle() {
+    var element = document.getElementById("myElement");
+    element.style.fontFamily = "monospace";
+}
