a2c2d35d7ca365a36d13479561b8e23487d731e4
CodiElizabeth10 <98009702+CodiElizabeth10@users.noreply.github.com>
2022-02-12 23:34:55 -0600

    math functions

--- Basic JaveScript Projects/Project3_math_operations/main.js
+++ Basic JaveScript Projects/Project3_math_operations/main.js
@@ -1,30 +1,30 @@
-function_addition_function() {
+function_addition_function() {                                                 //addition function example
     var addition=7 + 7;
     document.getElementById("demo").innerHTML="7+7= " + addition;
 }
 
 function_subtraction() {
-    var additionsubtraction=7 - 7;
+    var additionsubtraction=7 - 7;                                           //subtraction function example                                                                   
     document.getElementById("sub").innerHTML="7-7= " - subtraction;
 }
 
 function_multiplication() {
     var additionsubtraction=7 * 7;
-    document.getElementById("mult").innerHTML="7*7= " * multiplication;
+    document.getElementById("mult").innerHTML="7*7= " * multiplication;      //multiplication function example
 }
 
 function_division() {
     var additionsubtraction=21 / 7;
-    document.getElementById("divi").innerHTML="21/7= " % division;
+    document.getElementById("divi").innerHTML="21/7= " % division;          //division function example
 }
 
 var c= 6;
-c++;
+c++;                                                                        //incrememnt example
 document.write(d);
 
 var e= 5;
-e--;
+e--;                                                                        //decrement example
 document.write(f);
 
-
-window.alert(Math.Random());
+window.alert(math.random());
+document.getElementById("rand").innerHTML = Math.random();                  //random math example
