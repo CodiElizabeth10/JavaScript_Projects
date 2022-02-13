5757969c84fa195d1b91af107eaacc01eb649d7c
CodiElizabeth10 <98009702+CodiElizabeth10@users.noreply.github.com>
2022-02-12 19:16:58 -0600

    Pushing 2/12/22

---
+++ Basic JaveScript Projects/JS/main.js
@@ -0,0 +1,22 @@
+var name = prompt ("What is your name?");    //this is my prompt template
+alert ("Hello " + name + "!");
+
+document.write("Hi!<br><br>"); //this is an example of using document.write
+
+var lightseagreen = "<span style='color:#20b2aa'>Color line just so I could practice using it</span> <br><br>"; //This is my color text
+document.write(lightseagreen);
+
+var x = "<span style='color:#800080'>This is my practice JavaScript page</span> <br><br>"; //this is my assigning text to a variable example--- and using a different color as more practice
+document.write(x);
+
+document.write( "I asked my mom, \"Can you bring me some lunch?\" <br> She responded, \"yes, what would you like?\"<br><br>"); //this is my example on how to use quotes within quotes
+
+document.write("\" Be who you are and say what you feel, "+" because those who mind don\'t matter and those who matter don\'t mind. \" "+"-Dr Seuss <br><br>"); //this is my other quotes example with how to use apostrophes
+
+var b= "Hello," + " am I doing my concatenate string correctly?<br><br>" //this is my concatenate string example
+document.write(b);
+
+var Family = "Smith", Dad = "Jeremy <br><br>", Mom = "Hailey <br><br> ", Daughter = "Mia <br><br> ", Son = "Josh <br><br>"; //this is my var family example
+document.write(Dad);
+
+document.write(3 + 6); //this is my expressions example
---
+++ Basic JaveScript Projects/JS/var_family_template.js
@@ -0,0 +1,2 @@
+var family = "The Smiths", Dad = "Jeremy", Mom = "Liz", Daughter = "Hailey" Son = "Josh";
+document.write(Dad);
